0a9269b7b3ef7056d6fcb18ddd065242f04fe02a
Stephen Tudor <smtudor@gmail.com>
2014-12-17 12:16:59 -0500

    Add emacs to app install list ;)

--- install-03-apps.sh
+++ install-03-apps.sh
@@ -4,11 +4,11 @@ yaourt -S xorg xorg-xinit haskell-xmonad haskell-xmonad-contrib \
     haskell-xmonad-extras rxvt-unicode urxvt-perls-git dmenu numlockx xxkb \
     slock openssh chromium rsync jdk7-openjdk apache-ant maven lftp unzip \
     unrar tree subversion alsa-utils flashplugin ntp ranger chromium-libpdf \
-	glances aspell aspell-en ispell bc colordiff colorsvn fasd dmidecode \
+    glances aspell aspell-en ispell bc colordiff colorsvn fasd dmidecode \
     elinks htop iftop lesspipe highlight ncdu feh hwinfo pydf atool libcaca \
     dzen2 mplayer tcl tk lnav ntfs-3g cloc zathura zathura-pdf-poppler dtrx \
     dosfstools ntfsprogs ctags gvim fzf-git cabal-install the_silver_searcher \
-    wmctrl haskell-hlint fish alsamixer
+    wmctrl haskell-hlint fish alsamixer emacs
 
 mkdir -p ~/.config/ranger
 ranger --copy-config=scope
