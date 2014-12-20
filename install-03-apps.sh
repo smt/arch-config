e5abb74ab14cd04e48019439c955b8fc8f78e3fa
Stephen Tudor <smtudor@gmail.com>
2014-12-20 17:10:39 -0500

    Remove packages
    Some packages I won't use, and others had problems with installation, causing entire script to fail.

--- install-03-apps.sh
+++ install-03-apps.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 yaourt -S xorg xorg-xinit haskell-xmonad haskell-xmonad-contrib \
-    haskell-xmonad-extras rxvt-unicode urxvt-perls-git dmenu numlockx xxkb \
-    slock openssh chromium rsync jdk7-openjdk apache-ant maven lftp unzip \
-    unrar tree subversion alsa-utils flashplugin ntp ranger chromium-libpdf \
-    glances aspell aspell-en ispell bc colordiff colorsvn fasd dmidecode \
-    elinks htop iftop lesspipe highlight ncdu feh hwinfo pydf atool libcaca \
-    dzen2 mplayer tcl tk lnav ntfs-3g cloc zathura zathura-pdf-poppler dtrx \
-    dosfstools ntfsprogs ctags gvim fzf-git cabal-install the_silver_searcher \
-    wmctrl haskell-hlint fish alsamixer emacs
+    rxvt-unicode urxvt-perls-git dmenu numlockx xxkb slock openssh \
+    chromium rsync unzip unrar tree subversion alsa-utils flashplugin \
+    ntp ranger glances aspell aspell-en ispell bc colordiff colorsvn fasd \
+    dmidecode elinks htop iftop lesspipe highlight ncdu feh hwinfo pydf\
+    atool libcaca dzen2 mplayer tcl tk lnav cloc zathura \
+    zathura-pdf-poppler dtrx ctags gvim fzf-git cabal-install \
+    the_silver_searcher wmctrl haskell-hlint fish emacs
 
 mkdir -p ~/.config/ranger
 ranger --copy-config=scope
